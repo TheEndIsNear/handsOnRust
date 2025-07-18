7b31a5f0dece4b31c381f80ec20f046e6c565e9f
TheEndIsNear <2172212+TheEndIsNear@users.noreply.github.com>
2025-07-18 14:07:51 -0600

    refactor to use iter and struct for visitor data

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -8,11 +8,41 @@ fn what_is_your_name() -> String {
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
-    your_name
+    your_name.trim().to_lowercase()
+}
+
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting)
+    }
 }
 
 fn main() {
     println!("Hello, what's your name?");
     let name = what_is_your_name();
-    println!("Hello, {:?}", name)
+
+    let visitor_list = [
+        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
+        Visitor::new("steve", "Hi Steve. Your milk is in the fridge."),
+        Visitor::new("fred", "Wow, who invited Fred?"),
+    ];
+
+    let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+
+    match known_visitor {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("You are not on the visitor list. Please leave."),
+    }
 }
