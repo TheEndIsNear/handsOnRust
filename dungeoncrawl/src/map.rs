8c11bd8a59cec70e15283744ea3a18c2a9aab1ba
TheEndIsNear <2172212+TheEndIsNear@users.noreply.github.com>
2025-07-23 21:21:25 -0600

    add a player to the map and move them

--- dungeoncrawl/src/map.rs
+++ dungeoncrawl/src/map.rs
@@ -18,14 +18,10 @@ impl Map {
         }
     }
 
-    pub fn map_idx(&self, x: i32, y: i32) -> usize {
-        ((y * SCREEN_WIDTH) + x) as usize
-    }
-
     pub fn render(&self, ctx: &mut BTerm) {
         for y in 0..SCREEN_HEIGHT {
             for x in 0..SCREEN_WIDTH {
-                let idx = self.map_idx(x, y);
+                let idx = map_idx(x, y);
                 match self.tiles[idx] {
                     TileType::Floor => {
                         ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
@@ -37,4 +33,23 @@ impl Map {
             }
         }
     }
+
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point) && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
+    }
+
+    pub fn try_idx(&self, point: Point) -> Option<usize> {
+        if !self.in_bounds(point) {
+            None
+        } else {
+            Some(map_idx(point.x, point.y))
+        }
+    }
+}
+
+pub fn map_idx(x: i32, y: i32) -> usize {
+    ((y * SCREEN_WIDTH) + x) as usize
 }
